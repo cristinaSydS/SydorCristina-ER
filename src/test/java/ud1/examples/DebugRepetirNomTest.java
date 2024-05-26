963504698020b3e75c0657d5f12c2aa31b003cd8
Cris <cristinasydor@gmail.com>
2024-05-26 13:31:38 +0200

    commit

---
+++ src/test/java/ud1/examples/DebugRepetirNomTest.java
@@ -0,0 +1,12 @@
+package ud1.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DebugRepetirNomTest {
+
+    @Test
+    void main() {
+    }
+}
---
+++ src/test/java/ud5/examples/CarTest.java
@@ -0,0 +1,16 @@
+package ud5.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void accelerate() {
+    }
+
+    @Test
+    void testAccelerate() {
+    }
+}

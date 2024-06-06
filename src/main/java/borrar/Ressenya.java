1c45fafe76a67b2ab5eb87d4eea84987edeaee5d
Cris <cristinasydor@gmail.com>
2024-06-06 13:43:36 +0200

    adding 3

--- src/main/java/borrar/Ressenya.java
+++ src/main/java/borrar/Ressenya.java
@@ -1,41 +1,23 @@
 package borrar;
-/**
- * Representa una reseña de un libro
- * @author Cristina Sydor
- * @version 1.0, 27/05/2024
- */
+
 public class Ressenya {
-    /**
-     * Texto de una reseña de un libro
-     */
+
     private String text;
-    /**
-     * Puntuacion en la reseña
-     */
+
     private int puntuacio;
 
-    /**
-     * Crea una reseña especificando un texto y una puntuación
-     * @param text texto de una reseña
-     * @param puntuacio puntuación de la reseña
-     */
+
     public Ressenya(String text, int puntuacio) {
         this.text = text;
         this.puntuacio = puntuacio;
     }
 
-    /**
-     * Obtiene el texto de una reseña
-     * @return Texto de una reseña
-     */
+
     public String getText() {
         return text;
     }
 
-    /**
-     * Obtiene la puntuación de la reseña
-     * @return puntuación de la reseña
-     */
+    
     public int getPuntuacio() {
         return puntuacio;
     }
